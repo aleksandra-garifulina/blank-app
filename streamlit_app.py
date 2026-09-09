4a8c4c8a4cdad8626b885abfc17e25a3d32086ea
Aleksandra Garifulina <76571069+aleksandra-garifulina@users.noreply.github.com>
2026-09-09 09:58:45 +0000

    cleaned up the guessing part

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,16 +1,11 @@
 import streamlit as st
 
-st.title("🎈 My new app")
-st.write(
-    "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
-)
 
-
-st.title("Guess the author's (mine hehe) favourite dog breed")
+st.title("Guess the dog breed")
 
 items = ["labrador", "akita", "poodle", "podenco", "galgo", "german shepherd", "borzoi", "border collie", "Jack Russel terrier"]
 
-guess = st.selectbox("What do you think is my favourite?", items)
+guess = st.selectbox("What do you think is my favourite one?", items)
 
 if st.button("Submit guess"):
 
@@ -19,4 +14,6 @@ if st.button("Submit guess"):
         st.balloons()
     else:
         st.error("Nope :(, try again")
-        
+
+
+
