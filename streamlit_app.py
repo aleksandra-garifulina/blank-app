fd8480325555e9c8237458def826c60c9c5968b5
Aleksandra Garifulina <76571069+aleksandra-garifulina@users.noreply.github.com>
2026-09-09 09:26:52 +0000

    guess my fav dog breed v1

--- streamlit_app.py
+++ streamlit_app.py
@@ -5,5 +5,18 @@ st.write(
     "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
 )
 
-st.write("Hello, world!")
 
+st.title("Guess the author's (mine hehe) favourite dog breed")
+
+items = ["labrador", "akita", "poodle", "podenco", "galgo", "german shepherd", "borzoi", "border collie", "Jack Russel terrier"]
+
+guess = st.selectbox("What do you think is my favourite?", items)
+
+if st.button("Submit guess"):
+
+    if guess == "podenco":
+        st.success("Yess! 🐕")
+        st.balloons()
+    else:
+        st.error("Nope :(, try again")
+        
